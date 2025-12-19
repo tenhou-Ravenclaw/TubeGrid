589071846e5936e5b0f684d7d8b3109d70f1b5bd
aoilv99 <aoiiiiilv999@gmail.com>
2025-12-19 17:28:35 +0900

    feat: app.jsx動くようにしました

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Room from './room.jsx'; // Room.jsx を読み込む
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app-container">
+      {/* 現在は1画面完結なのでRoomのみ。
+         将来的にログイン画面などを作る場合はここで switch 文などで切り替えます
+      */}
+      <Room />
+    </div>
+  );
 }
 
-export default App
+export default App;
